f79377abcfc97cd9c6728c59e3c4b0866da451a5
QscarKIM <dong0294@naver.com>
2022-12-22 17:58:52 +0900

    22.12.22 02

--- 04.zoleolsearch.py
+++ 04.zoleolsearch.py
@@ -21,54 +21,44 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, TensorDataset, sampler
 
+device = torch.device('cuda:1') if torch.cuda.is_available() else torch.device('cpu')
+
+train = pd.read_csv("./data/df_train6.csv")
+test = pd.read_csv("./data/df_test6.csv")
+
+ae = pd.read_csv("./data/ae_values.csv")
+
+train2 = pd.concat([train, ae[:len(train)]], axis=1)
+test2 = pd.concat([test, ae[len(train):].reset_index(drop=True)], axis=1)
+y = torch.LongTensor(train2['class'].values)
+X = train2.drop(['id', 'class'], axis=1).to_numpy()
+X_test = test2.drop(['id', 'class'], axis=1).to_numpy()
+
 from xgboost import XGBClassifier, XGBRegressor
 from imblearn.over_sampling import SMOTE
-
-device = torch.device('cuda:2') if torch.cuda.is_available() else torch.device('cpu')
-print(device)
-
-random_seed = 5833
-
-def seed_everything(seed):
-    random.seed(seed)
-    np.random.seed(seed) 
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    
-def seed_worker(worker_id):
-    worker_seed = torch.initial_seed() % 2**32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
-
-seed_everything(seed=random_seed) # Seed 고정
-
-train = pd.read_csv("./data/df_train5.csv")
-test = pd.read_csv("./data/df_test5.csv")
-
-y = torch.LongTensor(train['class'].values)
-X = train.drop(['id', 'class', 'class_B', 'class_C'], axis=1).to_numpy()
-X_test = test.drop(['id', 'class', 'class_B', 'class_C'], axis=1).to_numpy()
-
 
 xgb_params = {
     'booster': 'gbtree',
     'grow_policy': 'lossguide',
     'max_depth': 0,
-    'learning_rate': 0.3,
-    'n_estimators': 50,
+    'learning_rate': 0.4,
+    'n_estimators': 30,
     'reg_lambda': 100,
     'subsample': 0.9,
     'num_parallel_tree': 1,
-    # 'rate_drop': 0.5
+    # 'rate_drop': 0.3
 }
 
-high = 0
-for k in tqdm(range(2, 5+1)) :
+def calc_f1_score(y_hat, data):
+    y_true = data.get_label()
+    y_hat = np.round(y_hat)
+    return 'f1', f1_score(y_true, y_hat, average="macro")
+
+high = 0.9
+for k in range(2, 5+1) :
     seeds = []
-    for _ in range(10) :
+    for _ in range(1000) :
+        
         while True :
             random_seed = np.random.randint(100000)
             if random_seed not in seeds :
@@ -78,13 +68,13 @@ for k in tqdm(range(2, 5+1)) :
                 continue
 
         y = train['class'].values
-        X = train.drop(['id', 'class', 'class_B', 'class_C'], axis=1).to_numpy()
-        X_test = test.drop(['id', 'class', 'class_B', 'class_C'], axis=1).to_numpy()
+        X = train.drop(['id', 'class'], axis=1).to_numpy()
+        X_test = test.drop(['id', 'class'], axis=1).to_numpy()
 
         skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=random_seed)
 
-        oof_val_preds = np.zeros((X.shape[0], 3))
-        oof_test_preds = np.zeros((X_test.shape[0], 3))
+        oof_val_preds = np.zeros((X.shape[0], 2))
+        oof_test_preds = np.zeros((X_test.shape[0], 2))
 
         # OOF
         for fold, (train_idx, valid_idx) in enumerate(skf.split(X, y)):
@@ -94,9 +84,8 @@ for k in tqdm(range(2, 5+1)) :
             # train, valid data 설정
             X_train, y_train = X[train_idx], y[train_idx]
             X_valid, y_valid = X[valid_idx], y[valid_idx]
-
-            strategy = {0:6900, 1:11400, 2:7900}
-            smote = SMOTE(random_state=random_seed, k_neighbors=7, sampling_strategy=strategy)
+            
+            smote = SMOTE(random_state=random_seed)
             X_train, y_train = smote.fit_resample(X_train, y_train)
 
             # 불균형 데이터 가중치 조정 값 => 음성(0) 타깃값 개수 / 양성(1) 타깃값 개수
@@ -111,26 +100,21 @@ for k in tqdm(range(2, 5+1)) :
                 random_state=random_seed,
                 n_jobs=-1
             )
-            xgb_model.fit(X_train, y_train, verbose=False)
+            xgb_model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], eval_metric=calc_f1_score, verbose=False)
 
             oof_test_preds += xgb_model.predict_proba(X_test) / skf.n_splits
             oof_val_preds[valid_idx] += xgb_model.predict_proba(X_valid)
-
-            # if fold == 1 :
-            #     pred = xgb_model.predict(X_test)
-            #     break
-
-            #model save
-            # xgb_model.save_model(f'./models/new_xgb_{skf.n_splits}_{fold}.json')
+            
             del [[X_train, y_train, X_valid, y_valid, xgb_model]]
             gc.collect()
 
         #     model score check
         preds = np.argmax(oof_val_preds, axis=1)
-        score =  f1_score(y, preds, average="macro")
+        score = f1_score(y, preds, average="macro")
+        print(score)
         if score > high :
             high = score
-            text = f'k: {k}, seed : {random_seed}, {score}'
+            text = f'k : {k}, seed : {random_seed} => {score}'
             print(text)
 
 print("="*80)
